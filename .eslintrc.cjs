a4a85fe00e696f242fb1c96102c54681f9aa969d
Kapil Madan <kapilmadan@microsoft.com>
2025-04-23 12:45:37 +0530

    vite react, tailwind css and eslint prettier configuration

---
+++ .eslintrc.cjs
@@ -0,0 +1,25 @@
+module.exports = {
+    env: {
+      browser: true,
+      es2021: true,
+    },
+    extends: [
+      'eslint:recommended',
+      'plugin:react/recommended',
+      'plugin:prettier/recommended',
+    ],
+    parserOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    plugins: ['react', 'prettier'],
+    rules: {
+      'prettier/prettier': 'error',
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  };
+  
